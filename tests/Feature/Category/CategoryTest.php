651b2964cf74d81fbdda62b7940517d44097a358
GabrielPSantana <gabrieldevti@gmail.com>
2025-11-20 22:24:04 -0300

    test: add test for index method returning categories page

---
+++ tests/Feature/Category/CategoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this->withoutExceptionHandling();
+});
+
+test('index returns a list of categories', function(){
+    Category::factory()->count(3)->create();
+
+    $response = $this->get(route('categories.index'));
+
+    $response->assertInertia(fn ($page) => 
+        $page->component('Category/Index')->has('categories', 3)
+    );
+});
+
